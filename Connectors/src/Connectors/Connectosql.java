74909f233d4b4be7c91a2aace974644445864312
Allend2311 <andayaallend64@gmail.com>
2024-11-12 18:45:31 +0800

    Progress of Xampp

---
+++ Connectors/src/Connectors/Connectosql.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Connectors;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class Connectosql {
+    private static String url ="jdbc:mysql://localhost:3306/fer_main";
+    private static String user ="root";
+    private static String pass ="";
+    
+    private static Connection connect;
+    private static Connectosql ctd;
+    
+     public Connectosql(){
+         connect = null;
+         try {
+             connect = DriverManager.getConnection(url, user, pass);
+             JOptionPane.showMessageDialog(null, "Connected Succesfull", "", 0);
+         
+         }catch (Exception e){
+             JOptionPane.showConfirmDialog(null, "Connection error", url, 0);
+         }
+    }
+     public static Connectosql getDBConnection() {
+        if (ctd== null){
+        ctd=new Connectosql();
+        }
+                
+        return ctd;
+    }
+     public static Connection getConnection(){
+         
+        
+        return connect;
+         
+        
+    }
+}
